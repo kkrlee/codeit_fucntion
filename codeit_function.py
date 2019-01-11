054f9fd1743560ec5a835c944f95ecbb4a76b62e
minjae <32402820+kkrlee@users.noreply.github.com>
2019-01-11 13:53:35 -0500

    first upload

---
+++ codeit_function.py
@@ -0,0 +1,28 @@
+def hello():
+    print("hello, world")
+    print("welcome to world")
+
+hello()
+
+
+def hello(name):
+    print("\nhello, %s" % (name))
+    print("welcome to world")
+
+hello("mj")
+hello("sy")
+
+
+
+def print_sum(a, b):
+    print(a + b)
+def print_product(a, b):
+    print(a * b)
+def print_residue(a, b):
+    print(a % b)
+def print_average(a, b, c):
+    print((a + b + c) / 3)
+print_sum(4, 2)
+print_product(4, 2)
+print_residue(5, 2)
+print_average(2,3,8)
